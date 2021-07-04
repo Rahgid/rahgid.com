84ef6f7b8fea0db3cd78a87b788f74c5e4db26fd
Dillon <58805003+Rahgid@users.noreply.github.com>
2021-07-03 23:20:23 -0700

    add polls app

--- mainsite/settings.py
+++ mainsite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls.apps.PollsConfig',
+    'ewb.apps.EwbConfig',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'mainsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
